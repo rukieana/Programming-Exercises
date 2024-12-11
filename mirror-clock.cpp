bf2ae652eb333cde73902eea13515abc0c2a76b9
rukieana <anahitamozaffari4@gmail.com>
2024-12-11 23:05:03 +0330

    Update mirror-clock.cpp

--- mirror-clock.cpp
+++ mirror-clock.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main(){
     int hour, minute , mirrorHour , mirrorMinute;
     cout << "Enter hour(s) and minute(s)";
-    cin >> hour >> minute;
-    mirrorHour = (12 - hour) % 12
-    mirrorMinute = (6 - minute) % 6
-    cout << setw(2) << setfill('0') << mirrorhour <<":";
-    cout << setw(2) << setfill('0') << mirrorminute;
+    cin >> mirrorHour >> mirrorMinute;
+    hour = (12 - mirrorHour) % 12
+    minute = (6 - mirrorMinute) % 6
+    cout << setw(2) << setfill('0') <<  hour <<":";
+    cout << setw(2) << setfill('0') << minute;
 
     return 0;
 }
