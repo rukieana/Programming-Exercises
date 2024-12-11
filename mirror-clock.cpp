37e8d425c705cd9eb56e598fba9f3489f3d8e8eb
rukieana <anahitamozaffari4@gmail.com>
2024-12-11 22:59:29 +0330

    Update mirror-clock.cpp

--- mirror-clock.cpp
+++ mirror-clock.cpp
@@ -1,28 +1,19 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.12.35424.110 d17.12
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "mirror-clock", "mirror-clock\mirror-clock.vcxproj", "{BAC9064A-5D87-44A5-83AF-EC80C3AF1F72}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{BAC9064A-5D87-44A5-83AF-EC80C3AF1F72}.Debug|x64.ActiveCfg = Debug|x64
-		{BAC9064A-5D87-44A5-83AF-EC80C3AF1F72}.Debug|x64.Build.0 = Debug|x64
-		{BAC9064A-5D87-44A5-83AF-EC80C3AF1F72}.Debug|x86.ActiveCfg = Debug|Win32
-		{BAC9064A-5D87-44A5-83AF-EC80C3AF1F72}.Debug|x86.Build.0 = Debug|Win32
-		{BAC9064A-5D87-44A5-83AF-EC80C3AF1F72}.Release|x64.ActiveCfg = Release|x64
-		{BAC9064A-5D87-44A5-83AF-EC80C3AF1F72}.Release|x64.Build.0 = Release|x64
-		{BAC9064A-5D87-44A5-83AF-EC80C3AF1F72}.Release|x86.ActiveCfg = Release|Win32
-		{BAC9064A-5D87-44A5-83AF-EC80C3AF1F72}.Release|x86.Build.0 = Release|Win32
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-EndGlobal
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    int hour, minute , mirrorHour , mirrorMinute;
+    cout << "Enter hour(s) and minute(s)";
+    cin >> hour >> minute;
+    mirrorHour = (12 - hour) % 12
+    mirrorMinute = (6 - minute) % 6
+    cout << setw(2) << setfill('0') << mirrorhour <<":";
+    cout << setw(2) << setfill('0') << mirrorminute;
+
+    return 0;
+}
+
+    
+
+        
